aeefdc0bce7355968c399f5848d2d7f373e1229c
Ishwarya <ishwaryavaradharajan18@gmail.com>
2024-04-07 13:22:29 +0530

    4_keys in_dict.py

---
+++ Code Folder/4_keys in_dict.py
@@ -0,0 +1,8 @@
+dict={}
+size=int(input("Enter the size:"))
+for i in range(size):
+    key=input("Enter the keys:")
+    value=input("Enter the values:")
+    dict[key]=value
+print(dict)
+print(dict.keys())
